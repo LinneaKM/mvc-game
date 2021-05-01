0bbe4acb18dbd4b6f608cb1e9f340aad50242f13
Linnéa Karnel Mjörnheim <linnea.karnel@gmail.com>
2021-05-01 19:48:22 +0200

    Created a layout to start from

--- view/yatzy.php
+++ view/yatzy.php
@@ -13,7 +13,39 @@ $message = $message ?? null;
 
 <p><?= $message ?></p>
 
-<p>DICE!!!</p>
-
-<p>DICEHAND!!!</p>
-
+<div class="yatzy-container">
+    <div class="game-container">
+        <form action="" method="">
+            <div class="dice-container">
+                <div class="unsaved-container">
+                    <h3 class="yatzy-header">Rolled Dice:</h3>
+                    <div class="unsaved-dice">
+                        <?php foreach ($unsaved as $index => $die) : ?>
+                            <div class="dice">
+                                <p><?= $die ?></p>
+                                <input type="checkbox" name="unsaved" value="<?= $index ?>">
+                            </div>
+                        <?php endforeach ?>
+                            </div>
+                    </div>
+                    <div class="saved-container">
+                        <h3 class="yatzy-header">Saved Dice:</h3>
+                        <div class="saved-dice">
+                        <?php foreach ($saved as $index => $die) : ?>
+                            <div class="dice">
+                                <p><?= $die ?></p>
+                                <input type="checkbox" name="saved" value="<?= $index ?>" checked>
+                            </div>
+                        <?php endforeach ?>
+                        </div>
+                    </div>
+            </div>
+            <div class="button-container">
+                <button>Klicka på mig!</button>
+            </div>
+        </form>
+    </div>
+    <div class="score-container">
+        <h3 class="yatzy-header">Score:</h3>
+    </div>
+</div>
